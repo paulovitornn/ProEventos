09d4fb9f0eb232a65ced7cea08f983d73e45bcad
Paulo Vitor <paulovitornn@gmail.com>
2021-09-30 19:15:40 -0300

    segundo commit

---
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] { 
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular 11 e .NET 5",
+                    Local = "Belo Horizonte",
+                    Lote = "1º lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto.png"
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular 11 e suas novidades",
+                    Local = "São Paulo",
+                    Lote = "2º lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                    ImagemURL = "fotoNova.png"
+                }
+            };
+        public EventoController(ILogger<EventoController> logger)
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where (evento => evento.EventoId == id);
+        }
+    }
+}
