210f701bd3566082b1e21abe2fcf7936dadb4489
Paulo Vitor <paulovitornn@gmail.com>
2021-10-02 17:35:10 -0300

    introducao ao dotnet entity framework core

--- backend/src/ProEventos.API/Controllers/EventoController.cs
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProEventos.API.Models;
+using ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers
 {
@@ -11,7 +12,7 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[] { 
+        /* public IEnumerable<Evento> _evento = new Evento[] { 
                 new Evento() {
                     EventoId = 1,
                     Tema = "Angular 11 e .NET 5",
@@ -31,20 +32,27 @@ namespace ProEventos.API.Controllers
                     ImagemURL = "fotoNova.png"
                 }
             };
-        public EventoController(ILogger<EventoController> logger)
+ */
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)  //ILogger<EventoController> logger
         {
+            _context = context;
         }
+
+        //public DataContext Context => _context;
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where (evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(
+                evento => evento.EventoId == id);
         }
     }
 }
