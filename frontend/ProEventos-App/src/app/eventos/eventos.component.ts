c7cfcbd24e644171950994563781e4e4301e752d
Paulo Vitor <paulovitornn@gmail.com>
2021-10-19 10:14:23 -0300

    secao angular pagina de listagem de eventos

--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-  public eventos: any
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg : number = 150;
+  marginImg : number = 2;
+  mostrarImagem : boolean = false;
+  private _filtroLista : string = '';
+
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+
+  public set filtroLista(filtro: string){
+    this._filtroLista = filtro;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista): this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -29,7 +54,10 @@ export class EventosComponent implements OnInit {
 
   getEvents() :void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.eventosFiltrados = response
+      },
       error => console.log(error)
     )
   }
