95d147df8d413a3cf848e8b229ca7112b6f52f20
Paulo Vitor <paulovitornn@gmail.com>
2023-05-18 13:06:22 -0300

    inclusao de formularios

--- frontend/ProEventos-App/src/app/componentes/nav/nav.component.ts
+++ frontend/ProEventos-App/src/app/componentes/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   isCollapsed = true;
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  showMenu(): Boolean {
+    return this.router.url != '/user/login';
+  }
 }
